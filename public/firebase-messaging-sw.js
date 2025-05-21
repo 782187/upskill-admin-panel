faea5f925c0e3e90144f1c7ad77fca9832b3c556
Avinash Shinde <ajs.ind2@gmail.com>
2025-05-21 21:41:34 +0530

    Updated keys

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
     apiKey: "AIzaSyDW05Js1HelvkEDDDEMn6osOXhFeSQvEZc",
     authDomain: "admindashboardnotification.firebaseapp.com",
     projectId: "admindashboardnotification",
-    storageBucket: "admindashboardnotification.appspot.com",
+    storageBucket: "admindashboardnotification.firebasestorage.app",
     messagingSenderId: "181121028054",
     appId: "1:181121028054:web:5eafad2baca63eb38c1e77"
 });
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,7 +5,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDW05Js1HelvkEDDDEMn6osOXhFeSQvEZc",
     authDomain: "admindashboardnotification.firebaseapp.com",
     projectId: "admindashboardnotification",
-    storageBucket: "admindashboardnotification.appspot.com",
+    storageBucket: "admindashboardnotification.firebasestorage.app",
     messagingSenderId: "181121028054",
     appId: "1:181121028054:web:5eafad2baca63eb38c1e77"
 };
