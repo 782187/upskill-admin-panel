4289e11d39ef2f3f0f8661d950b85f609a719a2d
Avinash Shinde <ajs.ind2@gmail.com>
2025-05-22 20:56:21 +0530

    Added storagebuckate

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,11 +1,11 @@
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyCP7Tbb66rUgfyvA50sqVPbObAw_A-Yezo",
   authDomain: "myprojectnotifications-8916d.firebaseapp.com",
   projectId: "myprojectnotifications-8916d",
-  storageBucket: "myprojectnotifications-8916d.firebaseapp.com",
+  storageBucket: "myprojectnotifications-8916d.appspot.com",
   messagingSenderId: "491410102542",
   appId: "1:491410102542:web:7517f5ca45cba13fc71f5f",
   measurementId: "G-DTX2C6PLV0"
@@ -14,11 +14,11 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
+  console.log('Received background message:', payload);
+  const notificationTitle = payload.notification?.title || 'Notification';
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/logo.png' 
+    body: payload.notification?.body || 'You have a new message.',
+    icon: '/logo.png'
   };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
