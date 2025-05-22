bac14d718cc3924e8fd1bc7eaa9ecd592359df1e
Avinash Shinde <ajs.ind2@gmail.com>
2025-05-22 22:18:05 +0530

    Added multiple file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
   apiKey: "AIzaSyCP7Tbb66rUgfyvA50sqVPbObAw_A-Yezo",
   authDomain: "myprojectnotifications-8916d.firebaseapp.com",
   projectId: "myprojectnotifications-8916d",
-  storageBucket: "myprojectnotifications-8916d.firebasestorage.app",
+  storageBucket: "myprojectnotifications-8916d.appspot.com", 
   messagingSenderId: "491410102542",
   appId: "1:491410102542:web:7517f5ca45cba13fc71f5f",
   measurementId: "G-DTX2C6PLV0"
--- src/components/FcmTokenHandler.jsx
+++ src/components/FcmTokenHandler.jsx
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCP7Tbb66rUgfyvA50sqVPbObAw_A-Yezo",
   authDomain: "myprojectnotifications-8916d.firebaseapp.com",
   projectId: "myprojectnotifications-8916d",
-  storageBucket: "myprojectnotifications-8916d.firebasestorage.app",
+  storageBucket: "myprojectnotifications-8916d.appspot.com",
   messagingSenderId: "491410102542",
   appId: "1:491410102542:web:7517f5ca45cba13fc71f5f",
   measurementId: "G-DTX2C6PLV0"
@@ -34,7 +34,7 @@ function sendTokenToBackend(token, adminId) {
     },
     body: new URLSearchParams({
       token: token,
-      adminId: adminId, 
+      adminId: adminId,
     }),
     credentials: "include",
   }).then(res => {
