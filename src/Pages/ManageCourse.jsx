64f8f56f974df8c0b145116aecb4bedfc0c42637
Avinash Shinde <ajs.ind2@gmail.com>
2025-05-27 18:48:51 +0530

    Added Course coded

--- src/Pages/ManageCourse.jsx
+++ src/Pages/ManageCourse.jsx
@@ -53,10 +53,10 @@ function ManageCourse() {
       <form onSubmit={handleSubmit} className="card p-4">
         <input type="text" placeholder="Title" className="form-control mb-2" value={title} onChange={(e) => setTitle(e.target.value)} required />
         <input type="text" placeholder="Duration" className="form-control mb-2" value={duration} onChange={(e) => setDuration(e.target.value)} required />
-        <textarea placeholder="Mode" className="form-control mb-2" value={mode} onChange={(e) => setMode(e.target.value)} required />
+        <input type="text" placeholder="Mode" className="form-control mb-2" value={mode} onChange={(e) => setMode(e.target.value)} required />
         <textarea placeholder="Description" className="form-control mb-2" value={description} onChange={(e) => setDescription(e.target.value)} required />
         <textarea placeholder="Topics" className="form-control mb-2" value={topics} onChange={(e) => setTopics(e.target.value)} required />
-        <input type="text" placeholder="Technology" className="form-control mb-2" value={technology} onChange={(e) => setTechnology(e.target.value)} required />
+        <textarea placeholder="Technology" className="form-control mb-2" value={technology} onChange={(e) => setTechnology(e.target.value)} required />
 
         <label>Course Image:</label>
         <input type="file" className="form-control mb-2" onChange={(e) => setCourseImage(e.target.files[0])} required />
