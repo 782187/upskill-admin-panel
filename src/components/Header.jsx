91446b29fc46db8c9ef0181d5eddcaa569385738
Avinash Shinde <ajs.ind2@gmail.com>
2025-07-08 15:45:19 +0530

    Added Data Of Notification

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 function Header() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [fetchEnquirey, updateEnquirey] = useState(0);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -12,7 +13,14 @@ function Header() {
       setIsDarkMode(savedMode === "dark");
       document.body.classList.toggle("dark-mode", savedMode === "dark");
     }
+    fetchEnquiries();
   }, []);
+
+  const fetchEnquiries = () => {
+    axios.get("https://upskill-server.onrender.com/get-enquiries")
+      .then((res) => updateEnquirey(res.data.length))
+      .catch(() => alert("Failed to fetch data"));
+  };
 
   const toggleTheme = () => {
     const newMode = !isDarkMode;
@@ -21,6 +29,9 @@ function Header() {
     document.body.classList.toggle("dark-mode", newMode);
   };
 
+  const fetchData = () => {
+    navigate("/enquiries");
+  }
   const handleLogout = () => {
     localStorage.removeItem("isLoggedIn");
     navigate("/login");
@@ -29,16 +40,17 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm position-sticky top-0 z-2 w-100">
       <div className="container-fluid d-flex justify-content-between align-items-center">
-      <img src="/logo.png" className="img-fluid" width="130px"/>
+        <img src="/logo.png" className="img-fluid" width="130px" />
         <div className="d-flex align-items-center gap-4">
           <button
+            onClick={fetchData}
             className="btn btn-outline-primary p-2 rounded-circle"
             title="Notifications"
             style={{ position: "relative" }}
           >
             <Bell className="text-dark" size={20} />
             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-              5
+              {fetchEnquirey}
             </span>
           </button>
 
